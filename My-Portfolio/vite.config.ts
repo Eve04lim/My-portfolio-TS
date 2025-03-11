81a22441baa8f57447312be2fd8fe5dc89555fd6
Eve04lim <k05y21@icloud.com>
2025-03-11 19:47:23 +0900

    デプロイ設定 #1

--- My-Portfolio/vite.config.ts
+++ My-Portfolio/vite.config.ts
@@ -1,7 +1,17 @@
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react()],
+  root: ".", // ルートディレクトリを明示的に指定
+  plugins: [react()],
+  build: {
+    outDir: "dist", // ビルド出力ディレクトリを指定
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
